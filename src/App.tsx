107a803391e82342df966199ec3c72c371339e11
jasonw <jasonmwild@gmail.com>
2025-02-14 19:13:26 -0800

    Removed React images

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <p>
-        DebtSolver FrontEnd in progress... coming soon!
+        DebtSolver front end in progress... coming soon!
       </p>
     </>
   )
