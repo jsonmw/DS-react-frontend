fbc160ea720e57a4e8bdf6feaa7f401fc631f99c
jasonw <jasonmwild@gmail.com>
2025-02-16 16:18:56 -0800

    Added basic navbar, messed with some CSS (not done)

---
+++ src/Components/Navbar.tsx
@@ -0,0 +1,57 @@
+import { FaBars } from "react-icons/fa";
+import { NavLink } from "react-router-dom";
+
+function Navbar() {
+  const isAuthenticated = false;
+  return (
+    <nav className="navbar navbar-expand-lg">
+      <div className="container">
+        <div
+          className="collapse navbar-collapse bg-dark "
+          id="navbarNav"
+        >
+          <div className="navbar-nav">
+            {!isAuthenticated ? ( // TODO: Remove negation once Authentication implemented
+              <>
+                <NavLink className="nav-link" to="/">
+                  Dashboard
+                </NavLink>
+                <NavLink className="nav-link" to="/new">
+                  New Debt
+                </NavLink>
+                <NavLink className="nav-link" to="/reports">
+                  Reports
+                </NavLink>
+              </>
+            ) : null}
+          </div>
+        </div>
+        <div className="d-flex" role="search">
+          {!isAuthenticated ? (
+            <>
+              <NavLink className="btn btn-sm" to="/login">
+                Login
+              </NavLink>
+              <NavLink className="btn btn-sm" to="/register">
+                Register
+              </NavLink>
+            </>
+          ) : null}
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <FaBars color="white" />
+          </button>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,12 +1,16 @@
-import React from 'react'
-
 function Welcome() {
   return (
-    <div>
-        <h1>Welcome to DebtSolver</h1>
-        <h5><em>Efficiently pay off your debt and unlock your finances</em></h5>
+    <div className="container welcome">
+      <div className="mt-2 text-center">
+        <div className="d-flex flex-column justify-content-center align-items-center pt-5">
+          <h1>Welcome to DebtSolver</h1>
+          <h6>
+            <em>Efficiently pay off your debt and unlock your finances</em>
+          </h6>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Welcome
+export default Welcome;
