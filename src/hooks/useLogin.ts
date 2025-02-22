a2814663432e5c44b4b48dc32faeefaa730a4c42
jasonw <jasonmwild@gmail.com>
2025-02-21 22:53:39 -0800

    Added useLogin hook

---
+++ src/hooks/useLogin.ts
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useAuthContext } from "./useAuthContext";
+import { AuthRequest } from "../model/AuthRequest";
+import { authenticate } from "../services/auth-service";
+
+export const useLogin = () => {
+  const [error, setError] = useState<string>("");
+  const [isLoading, setLoader] = useState<boolean>(false);
+  const navigate = useNavigate();
+  const { updateAuth } = useAuthContext();
+
+  const login = (authRequest: AuthRequest) => {
+    setLoader(true);
+    authenticate(authRequest)
+      .then((response) => {
+        console.log(response);
+        localStorage.setItem("user", JSON.stringify(response.data));
+        updateAuth(true);
+        navigate("/");
+      })
+      .catch((error) => {
+        if (
+          error.response &&
+          error.response.data &&
+          error.response.data.message
+        ) {
+          setError(error.response.data.message);
+        } else {
+          setError(error.message);
+        }
+      })
+      .finally(() => setLoader(false));
+  };
+
+  return { error, isLoading, login };
+};
