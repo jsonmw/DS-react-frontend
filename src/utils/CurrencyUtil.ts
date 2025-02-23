7e6dc620f7c27299fd6d888947f775dc992ae64e
jasonw <jasonmwild@gmail.com>
2025-02-22 23:07:59 -0800

    Added some utility classes for formatting currency, dates

---
+++ src/utils/CurrencyUtil.ts
@@ -0,0 +1,10 @@
+class CurrencyUtil {
+    static formatToUSD(amount: number) {
+        return new Intl.NumberFormat('en-US', {
+            style: 'currency',
+            currency: 'USD'
+        }).format(amount);
+    }
+}
+
+export default CurrencyUtil;
