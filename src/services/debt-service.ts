e6caeb6cbe6f692bfbef00ac57dc87164b55d963
jasonw <jasonmwild@gmail.com>
2025-02-23 19:05:49 -0800

    Added new debt service method

--- src/services/debt-service.ts
+++ src/services/debt-service.ts
@@ -4,3 +4,11 @@ import { Debt } from "../model/Debt"
 export const getDebts = () => {
     return apiClient.get<Debt[]>('/debts');
 }
+
+export const saveOrUpdateDebt = (debt: Debt) => {
+    if(debt.id !== undefined || debt.id !== null) {
+        return apiClient.put<Debt>(`/debts/${debt.id}`, debt);
+    }
+
+    return apiClient.post<Debt>(`/new`, debt);
+}
